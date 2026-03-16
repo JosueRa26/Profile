1879a423427f900290a450f0cb3e9e6a7607a85d
Josue <ramirezcespedes26@gmail.com>
2026-03-16 13:46:37 -0600

    Add detailed content to App component and create Home component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1>titulo</h1>
+      <h1>titulo</h1>
       <p>HOLA</p>
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        doloremque perferendis ut veritatis quidem. Exercitationem magnam ea
+        nulla eum nobis dolor doloremque blanditiis! Tenetur in non consectetur
+        quos fuga, doloremque iste excepturi dolore odit quod sapiente harum
+        natus, inventore illo. Qui rerum sint natus, aliquam aperiam, pariatur
+        deserunt ab esse eius similique voluptas delectus vitae deleniti vero,
+        ipsum adipisci dolorem. Odit aperiam expedita dicta quis illum nobis
+        suscipit deleniti officia numquam nemo blanditiis dolorem necessitatibus
+        debitis earum dignissimos quasi totam quas iure ipsam facere repellendus
+        itaque, dolore corrupti unde. Modi eum distinctio eaque voluptate iure,
+        praesentium ducimus delectus cumque odit.
+      </p>
+      <h2>prueba de la rama de develop</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
+        doloremque vel libero explicabo nam culpa incidunt molestiae officiis.
+        Dolore dolorem optio esse est reiciendis aliquam deleniti nisi minima
+        ut? Adipisci nesciunt non delectus deserunt ratione, dolorem aliquam
+        atque cum dolorum ut optio inventore minima enim facere natus laboriosam
+        unde expedita ullam, nihil neque aliquid! Ea a harum modi nam culpa
+        tempora, quis temporibus debitis sit quaerat eligendi repellendus
+        explicabo iste eius dolor nihil rem. Nostrum neque nemo distinctio sit,
+        iste quae reprehenderit minus dicta cupiditate aspernatur enim
+        recusandae repudiandae magni doloribus illo necessitatibus praesentium
+        voluptatem cumque amet tempora deserunt natus?
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
