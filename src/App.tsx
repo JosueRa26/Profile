9cde4fea3cafe4ec9f5fa9b6e3527d1d2bf8c27e
Josue <ramirezcespedes26@gmail.com>
2026-03-16 14:25:13 -0600

    cambio del h3

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
         recusandae repudiandae magni doloribus illo necessitatibus praesentium
         voluptatem cumque amet tempora deserunt natus?
       </p>
-      <h3>Probando errores</h3>
+      <h3>aaaaaaaaaa</h3>
     </>
   );
 }
