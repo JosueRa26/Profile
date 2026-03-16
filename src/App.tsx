1227bb14e611b9f9e9dff38e91f7a73b70d8d89b
Josue <ramirezcespedes26@gmail.com>
2026-03-16 14:56:42 -0600

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,7 @@ function App() {
       <h1>titulo del programa</h1>
       <p>HOLA</p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        doloremque perferendis ut veritatis quidem. Exercitationem magnam ea
-        nulla eum nobis dolor doloremque blanditiis! Tenetur in non consectetur
-        quos fuga, doloremque iste excepturi dolore odit quod sapiente harum
-        natus, inventore illo. Qui rerum sint natus, aliquam aperiam, pariatur
-        deserunt ab esse eius similique voluptas delectus vitae deleniti vero,
-        ipsum adipisci dolorem. Odit aperiam expedita dicta quis illum nobis
-        suscipit deleniti officia numquam nemo blanditiis dolorem necessitatibus
-        debitis earum dignissimos quasi totam quas iure ipsam facere repellendus
-        itaque, dolore corrupti unde. Modi eum distinctio eaque voluptate iure,
-        praesentium ducimus delectus cumque odit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fugiat.
       </p>
       <h2>prueba de la rama de develop</h2>
       <p>
