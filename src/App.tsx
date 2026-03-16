8420b4b9618350d9b845eab94dd4363ca7a329e4
Josue <ramirezcespedes26@gmail.com>
2026-03-16 14:37:57 -0600

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>titulo</h1>
+      <h1>titulo del programa</h1>
       <p>HOLA</p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
@@ -20,9 +20,19 @@ function App() {
       </p>
       <h2>prueba de la rama de develop</h2>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, asperiores.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
+        doloremque vel libero explicabo nam culpa incidunt molestiae officiis.
+        Dolore dolorem optio esse est reiciendis aliquam deleniti nisi minima
+        ut? Adipisci nesciunt non delectus deserunt ratione, dolorem aliquam
+        atque cum dolorum ut optio inventore minima enim facere natus laboriosam
+        unde expedita ullam, nihil neque aliquid! Ea a harum modi nam culpa
+        tempora, quis temporibus debitis sit quaerat eligendi repellendus
+        explicabo iste eius dolor nihil rem. Nostrum neque nemo distinctio sit,
+        iste quae reprehenderit minus dicta cupiditate aspernatur enim
+        recusandae repudiandae magni doloribus illo necessitatibus praesentium
+        voluptatem cumque amet tempora deserunt natus?
       </p>
-      <h3>aaaaaaaaaa</h3>
+      <h3>prueba de encabezado 3</h3>
     </>
   );
 }
