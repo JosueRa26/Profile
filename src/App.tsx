2b6e617a7ef2c8770041b6d11ea55f6680250d11
Josue <ramirezcespedes26@gmail.com>
2026-03-16 14:58:10 -0600

    remove unnecessary paragraphs and headings from App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,7 @@ function App() {
   return (
     <>
       <h1>titulo del programa</h1>
-      <p>HOLA</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fugiat.
-      </p>
-      <h2>prueba de la rama de develop</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, maxime!
-      </p>
-      <h3>prueba de encabezado 3</h3>
+      
     </>
   );
 }
