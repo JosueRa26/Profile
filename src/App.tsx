1cbc024b374be88c75b1e6c027e8af190818fe02
Josue <ramirezcespedes26@gmail.com>
2026-03-16 15:00:33 -0600

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>titulo del programa</h1>
-      <p>HOLA</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        doloremque perferendis ut veritatis quidem. Exercitationem magnam ea
-        nulla eum nobis dolor doloremque blanditiis! Tenetur in non consectetur
-        quos fuga, doloremque iste excepturi dolore odit quod sapiente harum
-        natus, inventore illo. Qui rerum sint natus, aliquam aperiam, pariatur
-        deserunt ab esse eius similique voluptas delectus vitae deleniti vero,
-        ipsum adipisci dolorem. Odit aperiam expedita dicta quis illum nobis
-        suscipit deleniti officia numquam nemo blanditiis dolorem necessitatibus
-        debitis earum dignissimos quasi totam quas iure ipsam facere repellendus
-        itaque, dolore corrupti unde. Modi eum distinctio eaque voluptate iure,
-        praesentium ducimus delectus cumque odit.
-      </p>
-      <h2>prueba de la rama de develop</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, maxime!
-      </p>
-      <h3>prueba de encabezado 3</h3>
-    </>
+    <h1>titulo del programa</h1>
+        </>
   );
 }
 
